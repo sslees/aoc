89d5f7b5ca10910cb9671420a008364cf43655b3
sslees <sam@sslees.com>
2020-12-01 22:40:00 -0700

    completed day02 and updated run config

---
+++ day02/day02a.py
@@ -0,0 +1,16 @@
+import itertools
+
+def main():
+    with open("input.txt") as f:
+        count = 0
+        for line in f.readlines():
+            line = line.rstrip()
+            policy, passwd = line.split(": ")
+            reps, letter = policy.split()
+            low, high = reps.split("-")
+            if int(low) <= passwd.count(letter) <= int(high):
+                count += 1
+    print(count)
+
+if __name__ == "__main__":
+    main()
---
+++ day02/day02b.py
@@ -0,0 +1,16 @@
+import itertools
+
+def main():
+    with open("test.txt") as f:
+        count = 0
+        for line in f.readlines():
+            line = line.rstrip()
+            policy, passwd = line.split(": ")
+            reps, letter = policy.split()
+            low, high = reps.split("-")
+            if bool(passwd[int(low) - 1] == letter) ^ bool(passwd[int(high) - 1] == letter):
+                count += 1
+    print(count)
+
+if __name__ == "__main__":
+    main()
