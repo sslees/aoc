671598dd8e296fa9f07bca437939d9e61a1f18de
sslees <sam@sslees.com>
2020-12-03 22:55:36 -0700

    completed day04

--- day04/day04a.py
+++ day04/day04a.py
@@ -1,22 +1,23 @@
 #! /bin/env python3
 
 def main():
-    dx = 3
-    dy = 1
-    with open("test.txt") as f:
-        m = [l.strip() for l in f.readlines()]
-    x = 0
-    y = 0
-    mx = len(m[1])
-    my = len(m)
+    with open("input.txt") as f:
+        d = f.read()
+    ps = d.split("\n\n")
     count = 0
-    while y < my:
-        if m[y][x] == '#':
+    for p in ps:
+        if ("byr" in p and
+            "iyr" in p and
+            "eyr" in p and
+            "hgt" in p and
+            "hcl" in p and
+            "ecl" in p and
+            "pid" in p
+            # p.contains("cid")
+        ):
             count += 1
-        x += dx
-        x = x % mx
-        y += dy
     print(count)
+
 
 if __name__ == "__main__":
     main()
--- day04/day04b.py
+++ day04/day04b.py
@@ -1,22 +1,33 @@
 #! /bin/env python3
 
+import re
+
+def valid(p):
+    byr = re.search(r"(?:^|\s+)byr:(\d{4})(?:$|\s+)", p)
+    iyr = re.search(r"(?:^|\s+)iyr:(\d{4})(?:$|\s+)", p)
+    eyr = re.search(r"(?:^|\s+)eyr:(\d{4})(?:$|\s+)", p)
+    hgt = re.search(r"(?:^|\s+)hgt:(\d+)(cm|in)(?:$|\s+)", p)
+    hcl = re.search(r"(?:^|\s+)hcl:(#[0-9a-f]{6})(?:$|\s+)", p)
+    ecl = re.search(r"(?:^|\s+)ecl:(amb|blu|brn|gry|grn|hzl|oth)(?:$|\s+)", p)
+    pid = re.search(r"(?:^|\s+)pid:(\d{9})(?:$|\s+)", p)
+    valid = bool(byr and iyr and eyr and hgt and hcl and ecl and pid)
+    if valid:
+        valid &= 1920 <= int(byr.group(1)) <= 2002
+        valid &= 2010 <= int(iyr.group(1)) <= 2020
+        valid &= 2020 <= int(eyr.group(1)) <= 2030
+        if hgt.group(2) == "cm":
+            valid &= 150 <= int(hgt.group(1)) <= 193
+        else:
+            valid &= 59 <= int(hgt.group(1)) <= 76
+    return valid
+
+
 def main():
-    dx = 1
-    dy = 2
-    with open("test.txt") as f:
-        m = [l.strip() for l in f.readlines()]
-    x = 0
-    y = 0
-    mx = len(m[1])
-    my = len(m)
-    count = 0
-    while y < my:
-        if m[y][x] == '#':
-            count += 1
-        x += dx
-        x = x % mx
-        y += dy
-    print(count)
+    with open("input.txt") as f:
+        d = f.read()
+    ps = d.split("\n\n")
+    print(len([p for p in ps if valid(p)]))
+
 
 if __name__ == "__main__":
     main()
