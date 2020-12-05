a8383ea06f78b346f182a6c90aa54fe439e55ec4
sslees <sam@sslees.com>
2020-12-04 22:29:33 -0700

    completed day05

--- day05/day05a.py
+++ day05/day05a.py
@@ -1,9 +1,14 @@
 #! /bin/env python3
 
+def id(code):
+    row = int(code[0:7].replace('F', '0').replace('B', '1'), 2)
+    col = int(code[7:10].replace('L', '0').replace('R', '1'), 2)
+    return row * 8 + col
+
 def main():
-    with open("test.txt") as f:
-        d = f.read()
-    print(d)
+    with open("input.txt") as f:
+        codes = [l.strip() for l in f.readlines()]
+    print(max([id(code) for code in codes]))
 
 
 if __name__ == "__main__":
--- day05/day05b.py
+++ day05/day05b.py
@@ -1,9 +1,17 @@
 #! /bin/env python3
 
+def id(code):
+    row = int(code[0:7].replace('F', '0').replace('B', '1'), 2)
+    col = int(code[7:10].replace('L', '0').replace('R', '1'), 2)
+    return row * 8 + col
+
 def main():
-    with open("test.txt") as f:
-        d = f.read()
-    print(d)
+    with open("input.txt") as f:
+        codes = [l.strip() for l in f.readlines()]
+    seats = [i for i in range(955)]
+    for s in [id(code) for code in codes]:
+        seats.remove(s)
+    print(seats)
 
 
 if __name__ == "__main__":
