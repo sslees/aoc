cc0ccaf3b99fa20bfc2169082ff48de9c0678f28
sslees <sam@sslees.com>
2020-12-06 23:42:23 -0700

    completed day07

--- day07/day07a.py
+++ day07/day07a.py
@@ -1,11 +1,36 @@
 #! /bin/env python3
 
+import re
+
+
+def containers(outers, inner, seen=[]):
+    ret = outers[inner]
+    for r in ret:
+        if r in outers and r not in seen:
+            seen.append(r)
+            ret.extend(containers(outers, r, seen))
+    return list(set(ret))
+
 
 def main():
-    with open("test.txt") as f:
-        d = [l.strip() for l in f.readlines()]
-    for l in d:
-        print(l)
+    with open("input.txt") as f:
+        lines = [l.strip() for l in f.readlines()]
+    rules = {}
+    for line in lines:
+        outer, inners = re.match(r"(.+) bags contain (.+)\.", line).groups()
+        inners = inners.split(", ")
+        for i, inner in enumerate(inners):
+            count, color = re.match(r"(\d+|no) (.+) bags?", inner).groups()
+            inners[i] = (int(count.replace("no", "0")), color)
+        rules[outer] = inners
+    outers = {
+        inner: []
+        for inner in set([inner[1] for inners in rules.values() for inner in inners])
+    }
+    for outer, inners in rules.items():
+        for inner in inners:
+            outers[inner[1]].append(outer)
+    print(len(containers(outers, "shiny gold")))
 
 
 if __name__ == "__main__":
--- day07/day07b.py
+++ day07/day07b.py
@@ -1,11 +1,28 @@
 #! /bin/env python3
 
+import re
+
+
+def contents(rules, outer):
+    return sum(
+        inner[0] + inner[0] * contents(rules, inner[1])
+        for inner in rules[outer]
+        if inner[1] in rules
+    )
+
 
 def main():
-    with open("test.txt") as f:
-        d = [l.strip() for l in f.readlines()]
-    for l in d:
-        print(l)
+    with open("input.txt") as f:
+        lines = [l.strip() for l in f.readlines()]
+    rules = {}
+    for line in lines:
+        outer, inners = re.match(r"(.+) bags contain (.+)\.", line).groups()
+        inners = inners.split(", ")
+        for i, inner in enumerate(inners):
+            count, color = re.match(r"(\d+|no) (.+) bags?", inner).groups()
+            inners[i] = (int(count.replace("no", "0")), color)
+        rules[outer] = inners
+    print(contents(rules, "shiny gold"))
 
 
 if __name__ == "__main__":
