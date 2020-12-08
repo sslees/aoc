cf4af9401fe619a655c49c0a2d40ed6ff066059e
sslees <sam@sslees.com>
2020-12-07 22:35:45 -0700

    completed day08

--- day08/day08a.py
+++ day08/day08a.py
@@ -2,10 +2,26 @@
 
 
 def main():
-    with open("test.txt") as f:
-        d = [l.strip() for l in f.readlines()]
-    for l in d:
-        print(l)
+    with open("input.txt") as f:
+        prog = [l.strip() for l in f.readlines()]
+    acc = 0
+    ptr = 0
+    seen = set()
+    while True:
+        if ptr in seen:
+            break
+        seen.add(ptr)
+        ins, val = prog[ptr].split()
+        val = int(val)
+        # print(ins, val)
+        if ins == "acc":
+            acc += val
+            ptr += 1
+        elif ins == "jmp":
+            ptr += val
+        else:
+            ptr += 1
+    print(acc)
 
 
 if __name__ == "__main__":
--- day08/day08b.py
+++ day08/day08b.py
@@ -1,11 +1,40 @@
 #! /bin/env python3
 
 
+def run(prog):
+    acc = 0
+    ptr = 0
+    seen = set()
+    while True:
+        if ptr in seen:
+            return None
+        seen.add(ptr)
+        ins, val = prog[ptr].split()
+        val = int(val)
+        if ins == "acc":
+            acc += val
+            ptr += 1
+        elif ins == "jmp":
+            ptr += val
+        elif ptr == len(prog) - 1:
+            return acc
+        else:
+            ptr += 1
+
+
 def main():
-    with open("test.txt") as f:
-        d = [l.strip() for l in f.readlines()]
-    for l in d:
-        print(l)
+    with open("input.txt") as f:
+        prog = [l.strip() for l in f.readlines()]
+    prog.append("nop 0")
+    for i in range(len(prog)):
+        tmp = prog.copy()
+        if tmp[i].split()[0] == "jmp":
+            tmp[i] = "nop " + tmp[i].split()[1]
+        elif tmp[i].split()[0] == "nop":
+            tmp[i] = "jmp " + tmp[i].split()[1]
+        else:
+            continue
+        print(run(tmp))
 
 
 if __name__ == "__main__":
