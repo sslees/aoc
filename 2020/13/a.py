bf792bcad2607629d14118a331372fa409afbb48
sslees <sam@sslees.com>
2020-12-13 01:39:45 -0700

    completed day13

--- 2020/13/a.py
+++ 2020/13/a.py
@@ -1,11 +1,20 @@
 #! /bin/env python3
 
+import itertools
+
 
 def main():
-    with open("test.txt") as f:
-        d = [l.strip() for l in f.readlines()]
-    for l in d:
-        print(l)
+    with open("input.txt") as f:
+        fst = int(f.readline())
+        snd = set([l.strip() for l in f.readline().split(",")])
+        snd.remove("x")
+        snd = set(map(int, snd))
+    print(fst, snd)
+    nxt = {}
+    for e in snd:
+        nxt[e] = (fst // e + 1) * e - fst
+    bus = sorted(nxt, key=lambda e: nxt[e])[0]
+    print(bus * nxt[bus])
 
 
 if __name__ == "__main__":
--- 2020/13/b.py
+++ 2020/13/b.py
@@ -1,11 +1,26 @@
 #! /bin/env python3
 
+import itertools
+
 
 def main():
-    with open("test.txt") as f:
-        d = [l.strip() for l in f.readlines()]
-    for l in d:
-        print(l)
+    with open("input.txt") as f:
+        sched = [l.strip() for l in f.readlines()[1].split(",")]
+    rs = {int(n): (int(n) - i) % int(n) for i, n in enumerate(sched) if n != "x"}
+    ns = list(sorted(rs))
+
+    n = ns.pop()
+    r = rs[n]
+    while ns:
+        n2 = ns[-1]
+        r2 = rs[n2]
+        for t in itertools.count(r, n):
+            if t % n2 == r2:
+                r = t
+                n *= n2
+                ns.pop()
+                break
+    print(r)
 
 
 if __name__ == "__main__":
