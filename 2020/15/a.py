b755a2447d598f9f3453b6774fbfc272cfe97c34
sslees <sam@sslees.com>
2020-12-14 22:36:45 -0700

    completed day15

--- 2020/15/a.py
+++ 2020/15/a.py
@@ -3,9 +3,24 @@
 
 def main():
     with open("e1.txt") as f:
-        d = [l.strip() for l in f.readlines()]
-    for l in d:
-        print(l)
+        d = [int(l.strip()) for l in f.readline().split(",")]
+    more = {}
+    when = {}
+    for i, e in enumerate(d):
+        if e in when:
+            more[e] = when[e]
+        when[e] = i + 1
+    last = d[-1]
+    for i in range(len(d) + 1, 2021):
+        if last not in more:
+            say = 0
+        else:
+            say = when[last] - more[last]
+        if say in when:
+            more[say] = when[say]
+        when[say] = i
+        last = say
+    print(say)
 
 
 if __name__ == "__main__":
