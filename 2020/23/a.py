af28ab4ceca8a8e26283ccc228279f402586c6ef
sslees <sam@sslees.com>
2021-01-02 23:21:11 -0700

    completed day23

--- 2020/23/a.py
+++ 2020/23/a.py
@@ -2,10 +2,26 @@
 
 
 def main():
-    with open("e1.txt") as f:
-        data = [l.strip() for l in f.readlines()]
-    for l in data:
-        print(l)
+    with open("input.txt") as f:
+        cups = [int(c) for c in f.readline().strip()]
+    n = len(cups)
+    cur = cups[0]
+    for _ in range(100):
+        i = cups.index(cur)
+        pu = (cups * 2)[i + 1 : i + 4]
+        for c in pu:
+            cups.remove(c)
+        dest = cur - 1
+        while dest and dest in pu:
+            dest -= 1
+        if not dest:
+            dest = max(cups)
+        d = cups.index(dest) + 1
+        for c in reversed(pu):
+            cups.insert(d, c)
+        cur = cups[(cups.index(cur) + 1) % n]
+    i = cups.index(1)
+    print("".join(map(str, (cups * 2)[i + 1 : i + n])))
 
 
 if __name__ == "__main__":
--- 2020/23/b.py
+++ 2020/23/b.py
@@ -1,11 +1,42 @@
 #! /bin/env python3
 
+CUPS = 1_000_000
+MOVES = 10_000_000
+
+
+class Node:
+    def __init__(self, val, nxt):
+        self.val = val
+        self.nxt = nxt
+
+    def __repr__(self):
+        return "{}->{}".format(self.val, self.nxt.val)
+
 
 def main():
-    with open("e1.txt") as f:
-        data = [l.strip() for l in f.readlines()]
-    for l in data:
-        print(l)
+    with open("input.txt") as f:
+        cups = [int(c) for c in f.readline().strip()]
+    cups.extend(c for c in range(len(cups) + 1, CUPS + 1))
+    ndx = {}
+    cur = None
+    for c in cups[::-1]:
+        cur = Node(c, cur)
+        ndx[c] = cur
+    ndx[cups[-1]].nxt = ndx[cups[0]]
+    for _ in range(MOVES):
+        pu = cur.nxt
+        cur.nxt = pu.nxt.nxt.nxt
+        dst = cur.val - 1
+        while not dst or dst in (pu.val, pu.nxt.val, pu.nxt.nxt.val):
+            if not dst:
+                dst = CUPS + 1
+            dst -= 1
+        dst = ndx[dst]
+        pu.nxt.nxt.nxt = dst.nxt
+        dst.nxt = pu
+        cur = cur.nxt
+    cur = ndx[1].nxt
+    print(cur.val * cur.nxt.val)
 
 
 if __name__ == "__main__":
