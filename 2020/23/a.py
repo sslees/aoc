8136046eef28160e36d7792a8ff1f4b8f2eab437
sslees <sam@sslees.com>
2021-05-22 19:47:46 -0600

    crab cups 1.8x faster

--- 2020/23/a.py
+++ 2020/23/a.py
@@ -1,27 +1,29 @@
 #! /bin/env python3
 
+MOVES = 100
+
 
 def main():
     with open("input.txt") as f:
         cups = [int(c) for c in f.readline().strip()]
-    n = len(cups)
-    cur = cups[0]
-    for _ in range(100):
-        i = cups.index(cur)
-        pu = (cups * 2)[i + 1 : i + 4]
-        for c in pu:
-            cups.remove(c)
-        dest = cur - 1
-        while dest and dest in pu:
-            dest -= 1
-        if not dest:
-            dest = max(cups)
-        d = cups.index(dest) + 1
-        for c in reversed(pu):
-            cups.insert(d, c)
-        cur = cups[(cups.index(cur) + 1) % n]
-    i = cups.index(1)
-    print("".join(map(str, (cups * 2)[i + 1 : i + n])))
+    after = {}
+    cur = cups[-1]
+    for cup in cups:
+        after[cur] = cup
+        cur = cup
+    for _ in range(MOVES):
+        cur = after[cur]
+        up = [a := after[cur], b := after[a], c := after[b]]
+        after[cur] = after[c]
+        dest = cur
+        while dest == cur or dest in up:
+            dest = dest - 1 if dest > 1 else max(cups)
+        after[c] = after[dest]
+        after[dest] = a
+    order = [after[1]]
+    for _ in range(len(cups) - 2):
+        order.append(after[order[-1]])
+    print("".join(map(str, order)))
 
 
 if __name__ == "__main__":
--- 2020/23/b.py
+++ 2020/23/b.py
@@ -4,39 +4,27 @@ CUPS = 1_000_000
 MOVES = 10_000_000
 
 
-class Node:
-    def __init__(self, val, nxt):
-        self.val = val
-        self.nxt = nxt
-
-    def __repr__(self):
-        return "{}->{}".format(self.val, self.nxt.val)
-
-
 def main():
     with open("input.txt") as f:
         cups = [int(c) for c in f.readline().strip()]
-    cups.extend(c for c in range(len(cups) + 1, CUPS + 1))
-    ndx = {}
-    cur = None
-    for c in cups[::-1]:
-        cur = Node(c, cur)
-        ndx[c] = cur
-    ndx[cups[-1]].nxt = ndx[cups[0]]
+    after = {}
+    cur = CUPS
+    for cup in cups:
+        after[cur] = cup
+        cur = cup
+    for cup in range(max(cups) + 1, CUPS + 1):
+        after[cur] = cup
+        cur = cup
     for _ in range(MOVES):
-        pu = cur.nxt
-        cur.nxt = pu.nxt.nxt.nxt
-        dst = cur.val - 1
-        while not dst or dst in (pu.val, pu.nxt.val, pu.nxt.nxt.val):
-            if not dst:
-                dst = CUPS + 1
-            dst -= 1
-        dst = ndx[dst]
-        pu.nxt.nxt.nxt = dst.nxt
-        dst.nxt = pu
-        cur = cur.nxt
-    cur = ndx[1].nxt
-    print(cur.val * cur.nxt.val)
+        cur = after[cur]
+        up = [a := after[cur], b := after[a], c := after[b]]
+        after[cur] = after[c]
+        dest = cur
+        while dest == cur or dest in up:
+            dest = dest - 1 if dest > 1 else CUPS
+        after[c] = after[dest]
+        after[dest] = a
+    print(after[1] * after[after[1]])
 
 
 if __name__ == "__main__":
