c9b1d84e3710920b8bbb70e02b7f9eb3731b36cb
sslees <sam@sslees.com>
2021-06-18 17:48:36 -0600

    donut maze part b

--- 2019/20/a.py
+++ 2019/20/a.py
@@ -12,31 +12,31 @@ def main():
     for r, line in enumerate(data):
         for c, ch in enumerate(line):
             if ch == ".":
-                maze.add_node((r, c), label=ch)
+                maze.add_node((r, c))
                 for nbr in [(r - 1, c), (r, c - 1)]:
                     if nbr in maze:
                         maze.add_edge((r, c), nbr, weight=1)
     for n, deg in list(maze.degree):
-        if deg == 2 and maze.nodes[n]["label"] == ".":
+        if deg == 2:
             u, v = maze.neighbors(n)
             wt = maze.edges[u, n]["weight"] + maze.edges[n, v]["weight"]
             maze.add_edge(u, v, weight=wt)
             maze.remove_node(n)
-    lbls = defaultdict(list)
+    portals = defaultdict(list)
     for r, line in enumerate(data):
         for m in re.finditer(r"(\w\w)\.", line):
-            lbls[m[1]].append((r, m.end() - 1))
+            portals[m[1]].append((r, m.end() - 1))
         for m in re.finditer(r"\.(\w\w)", line):
-            lbls[m[1]].append((r, m.start()))
+            portals[m[1]].append((r, m.start()))
     for c, line in enumerate(map("".join, zip(*data))):
         for m in re.finditer(r"(\w\w)\.", line):
-            lbls[m[1]].append((m.end() - 1, c))
+            portals[m[1]].append((m.end() - 1, c))
         for m in re.finditer(r"\.(\w\w)", line):
-            lbls[m[1]].append((m.start(), c))
-    for lbl in lbls.values():
-        if len(lbl) == 2:
-            maze.add_edge(*lbl, weight=1)
-    print(nx.shortest_path_length(maze, lbls["AA"][0], lbls["ZZ"][0], "weight"))
+            portals[m[1]].append((m.start(), c))
+    for tiles in portals.values():
+        if len(tiles) == 2:
+            maze.add_edge(*tiles, weight=1)
+    print(nx.shortest_path_length(maze, portals["AA"][0], portals["ZZ"][0], "weight"))
 
 
 if __name__ == "__main__":
---
+++ 2019/20/b.py
@@ -0,0 +1,63 @@
+#! /bin/env python3
+
+from itertools import count, product
+import networkx as nx
+import re
+
+
+def main():
+    with open("input.txt") as f:
+        data = [l[:-1] for l in f.readlines()]
+    base = nx.Graph()
+    for r, line in enumerate(data):
+        for c, ch in enumerate(line):
+            if ch == ".":
+                base.add_node((r, c))
+                for nbr in [(r - 1, c), (r, c - 1)]:
+                    if nbr in base:
+                        base.add_edge((r, c), nbr, weight=1)
+    for n, deg in list(base.degree):
+        if deg == 2:
+            u, v = base.neighbors(n)
+            wt = base.edges[u, n]["weight"] + base.edges[n, v]["weight"]
+            base.add_edge(u, v, weight=wt)
+            base.remove_node(n)
+    inner = {}
+    outer = {}
+    for r, line in enumerate(data):
+        for m in re.finditer(r" (\w\w)\.", line):
+            inner[m[1]] = (r, m.end() - 1)
+        for m in re.finditer(r"\.(\w\w) ", line):
+            inner[m[1]] = (r, m.start())
+        for m in re.finditer(r"^(\w\w)\.", line):
+            outer[m[1]] = (r, m.end() - 1)
+        for m in re.finditer(r"\.(\w\w)$", line):
+            outer[m[1]] = (r, m.start())
+    for c, line in enumerate(map("".join, zip(*data))):
+        for m in re.finditer(r" (\w\w)\.", line):
+            inner[m[1]] = (m.end() - 1, c)
+        for m in re.finditer(r"\.(\w\w) ", line):
+            inner[m[1]] = (m.start(), c)
+        for m in re.finditer(r"^(\w\w)\.", line):
+            outer[m[1]] = (m.end() - 1, c)
+        for m in re.finditer(r"\.(\w\w)$", line):
+            outer[m[1]] = (m.start(), c)
+    maze = nx.Graph()
+    portals = set(inner) & set(outer)
+    for depth in count():
+        for u, v, wt in base.edges.data("weight"):
+            maze.add_edge((depth, u), (depth, v), weight=wt)
+        if depth:
+            for lbl in portals:
+                maze.add_edge((depth - 1, inner[lbl]), (depth, outer[lbl]), weight=1)
+        try:
+            edge = (0, outer["AA"]), (0, outer["ZZ"])
+            len = nx.shortest_path_length(maze, *edge, "weight")
+            print(len)
+            break
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    main()
