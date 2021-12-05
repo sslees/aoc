d9348fd755f02dc59f1a1b2034d197a150ac4159
sslees <sam@sslees.com>
2021-12-04 23:04:24 -0700

    2021/04

---
+++ 2021/04/b.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import aocd
+
+
+def solve(data: str):
+    boards = data.split("\n\n")
+    calls = [int(s) for s in boards.pop(0).split(",")]
+    whens = {e: i for i, e in enumerate(calls)}
+    rounds = []
+    for i, board in enumerate(boards):
+        board = [[int(s) for s in l.split()] for l in board.splitlines()]
+        boards[i] = [e for r in board for e in r]
+        board += list(map(list, zip(*board)))
+        rounds.append(min(max(map(whens.get, r)) for r in board))
+    round = max(rounds)
+    called = calls[: round + 1]
+    return sum(set(boards[rounds.index(round)]) - set(called)) * called[-1]
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        data = f.read().rstrip("\r\n")
+    answer = solve(data)
+    print(answer)
+    aocd.submit(answer, part="b", day=4, year=2021)
